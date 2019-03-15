30578a44326ccf5bde6c64244cc9c8a9fbb3e50f
Pom4H <1@rom4.ru>
2019-03-16 02:31:06 +0300

    fix: Remove git error package

--- src/components/error/ErrorExtractor.ts
+++ src/components/error/ErrorExtractor.ts
@@ -1,20 +1,11 @@
-import { HttpErrorCode } from './HttpErrorCode';
-import { ValidationErrorFactory } from './ValidationErrorFactory';
 import { SerializedError, SerializableError } from './SerializableError';
 import { InternalServerError } from './InternalServerError';
-
-enum ErrorCode {
-    INTERNAL_SERVER_ERROR = 'InternalServerError',
-    VALIDATION_ERROR = 'ValidationError'
-}
 
 class ErrorExtractor {
   public extract(error: Error): SerializedError {
     let serializedError: SerializedError;
     if (this.isDomainError(error)) {
       serializedError = this.createCoreError(error);
-    } else if (this.isValidationError(error)) {
-      serializedError = this.createValidationError(error);
     } else {
       serializedError = this.createInternalServerError(error);
     }
@@ -26,25 +17,14 @@ class ErrorExtractor {
     return errorData;
   }
 
-  protected createValidationError(error: Error): SerializedError {
-    const validationErrorItems = ValidationErrorFactory.createValidationErrors((error as any).errors);
-    return {
-      code: ErrorCode.VALIDATION_ERROR,
-      message: 'Invalid body',
-      httpCode: HttpErrorCode.UNPROCESSABLE_ENTITY,
-      errorData: {
-        fields: validationErrorItems
-      }
-    };
+  protected createValidationError(error: SerializableError): SerializedError {
+    const errorData = error.serialize();
+    return errorData;
   }
 
   protected createInternalServerError(error: Error): SerializedError {
     const internalServerError = InternalServerError.fromError(error);
     return internalServerError.serialize();
-  }
-
-  private isValidationError(error: Error): boolean {
-    return Boolean((error as any).errors);
   }
 
   private isDomainError(error: Error): error is SerializableError {
