43ee0df2e0191fc788da904556aed1c38723efd7
Adil <adilserkerov@gmail.com>
2019-03-19 23:01:56 +0300

    change dev conf

--- src/inf/wrappers/StatsServiceWrapper.ts
+++ src/inf/wrappers/StatsServiceWrapper.ts
@@ -3,7 +3,7 @@ import { ServiceWrapper } from '../../components/restClient/ServiceWrapper';
 
 export class StatsServiceWrapper extends ServiceWrapper {
   public async sendStats(gameStats: GameStats): Promise<void> {
-    await this.post('/stats', { gameStats });
+    await this.put('/game', { gameStats });
   }
 
   protected get serviceName(): string {
