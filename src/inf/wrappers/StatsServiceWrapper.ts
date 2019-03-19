9ffc17ac32b2e5520285ea61646d3fe86866d472
Adil <adilserkerov@gmail.com>
2019-03-19 23:57:40 +0300

    fix config and wrapper

--- src/inf/wrappers/StatsServiceWrapper.ts
+++ src/inf/wrappers/StatsServiceWrapper.ts
@@ -3,7 +3,9 @@ import { ServiceWrapper } from '../../components/restClient/ServiceWrapper';
 
 export class StatsServiceWrapper extends ServiceWrapper {
   public async sendStats(gameStats: GameStats): Promise<void> {
-    await this.put('/game', { gameStats });
+    console.log(gameStats);
+    console.log(JSON.stringify(gameStats, null, 3));
+    await this.put('/game', { ...gameStats });
   }
 
   protected get serviceName(): string {
