3012860cf0c50c96507bb6876bfca068f00d66b5
Pom4H <1@rom4.ru>
2019-03-22 02:18:33 +0300

    fix: typescript error

--- src/inf/wrappers/StatsServiceWrapper.ts
+++ src/inf/wrappers/StatsServiceWrapper.ts
@@ -2,6 +2,7 @@ import { GameStats } from '../../domain/game/GameStats';
 import { ServiceWrapper } from '../../components/restClient/ServiceWrapper';
 
 export class StatsServiceWrapper extends ServiceWrapper {
+  protected serviceName: string;
   public async sendStats(gameStats: GameStats): Promise<void> {
     await this.put('/game', { ...gameStats });
   }
